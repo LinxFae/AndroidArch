a9f3b120afabc226590860663dc7485e3ad402af
sudiptadutta <ir2pid@gmail.com>
2018-10-29 08:40:47 +0100

    refactoring details

--- app/src/main/java/com/noisyninja/androidlistpoc/views/detail/DetailActivity.kt
+++ app/src/main/java/com/noisyninja/androidlistpoc/views/detail/DetailActivity.kt
@@ -2,14 +2,21 @@ package com.noisyninja.androidlistpoc.views.detail
 
 
 import android.databinding.DataBindingUtil
+import android.databinding.DataBindingUtil.setContentView
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import com.noisyninja.androidlistpoc.NinjaApp
 import com.noisyninja.androidlistpoc.R
 import com.noisyninja.androidlistpoc.databinding.ActivityDetailBinding
+import com.noisyninja.androidlistpoc.model.Me
 
 
-open class DetailActivity : AppCompatActivity() {
+open class DetailFragment : Fragment(), IDetailActivity {
+
+    override fun setMe(result: Me) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     lateinit var mIDetailPresenter: IDetailPresenter
 
